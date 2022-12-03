405db5f2cdd88886fa18330b58015c1e992b45d3
Bruno Felipe <brunofelipehp8@gmail.com>
2022-12-02 22:20:14 -0300

    mobile visual structure

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';  
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
   name: string;
@@ -7,6 +12,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
   user: UserProps;
+  isUserLoading: boolean;
   signIn: () => Promise<void>;
 }
 
@@ -17,19 +23,44 @@ interface AuthProviderProps {
  export const AuthContext = createContext({} as AuthContextDataProps)
 
  export function AuthContextProvider({ children }: AuthProviderProps){
+   const [user, UserProps] = useState<UserProps>({} as UserProps)
+  const [isUserLoading, setIsUserLoading] = useState(false)
 
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: '1094449304888-10gqjpslgmcqhcs7cr419cdeg6h42cqv.apps.googleusercontent.com',
+    redirectUri: AuthSession.makeRedirectUri({useProxy: true}),
+    scopes: ['profile', 'email']
+  });
+
+  
   async function signIn(){ 
-    console.log('Vamos logar');
+   try {
+    setIsUserLoading(true)
+    await promptAsync();
+   } catch (error) {
+    console.log(error);
+    throw error;
+   }finally {
+    setIsUserLoading(false)
+   }
     
   }
+
+  async function signInWithGoogle(accessToken: string){
+    console.log("TOKEN DE AUTENTICAÇÃO ===>", accessToken);
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken)
+    }
+  }, [response])
 
   return(
     <AuthContext.Provider value={{
       signIn,
-      user: {
-        name: 'Bruno',
-        avatarUrl: 'http://github.com/brunofelipehp.png'
-      }
+      isUserLoading,
+      user,
     }}>
       {children}
     </AuthContext.Provider>
