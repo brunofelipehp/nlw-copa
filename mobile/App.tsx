cd544de9a8c441dc3952a7b04094d12cf37c7362
Bruno Felipe <brunofelipehp8@gmail.com>
2022-12-07 23:37:07 -0300

    connecting app with back-end

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,7 +3,7 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@
 
 import { AuthContextProvider } from "./src/contexts/AuthContext";
 
-import { Pools } from './src/screens/Pools';
+import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
@@ -19,7 +19,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontLoaded ? <Pools /> : <Loading />}
+        {fontLoaded ? <SignIn /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
